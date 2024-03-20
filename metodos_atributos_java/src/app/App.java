029efd02249c7d7b13bc97581ca6a699cc3ca3ef
Cinthia Cavalheiro <Cinthia>
2024-03-20 20:24:34 -0300

    criado repo

---
+++ metodos_atributos_java/src/app/App.java
@@ -0,0 +1,12 @@
+package app;
+
+import package1.Media;
+
+import package1.Numbers;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Numbers.num (args);
+        Media.media (args);
+    }
+}
---
+++ metodos_atributos_java/src/package1/Media.java
@@ -0,0 +1,56 @@
+package package1;
+import java.util.Locale;
+import java.util.Scanner;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class Media {
+    public static void media (String[] args)throws Exception{
+        float num_1 = 0;
+        float num_2 = 0;
+        float num_3 = 0;
+        String nome;
+
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        Scanner input_user = new Scanner(System.in);
+
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
+        System.out.println(dtf.format(LocalDateTime.now()));
+
+        System.out.println("Olá caro usuário!");
+        System.out.println("Digite seu nome: ");
+        nome = input_user.nextLine();
+
+        System.out.println(nome +" Digite um número: ");
+        num_1 = input_user.nextInt();
+        while (num_1 < 0){
+            System.out.println(nome + " Digite um número inteiro > que 0: ");
+            num_1 = input_user.nextInt();
+        }
+
+        System.out.println(nome +" Digite um número: ");
+        num_2 = input_user.nextInt();
+        while (num_2 < 0){
+            System.out.println(nome + " Digite um número inteiro > que 0: ");
+            num_2 = input_user.nextInt();
+        }
+
+        System.out.println(nome +" Digite um número: ");
+        num_3 = input_user.nextInt();
+        while (num_3 < 0){
+            System.out.println(nome + " Digite um número inteiro > que 0: ");
+                num_3 = input_user.nextInt();
+        }      
+
+        double media_aritimetica = (num_1 + num_2 + num_3)/3;
+               
+        if (num_1 >= 0 && num_2 >= 0 && num_3 >= 0){
+            System.out.println(nome + " A média aritimética de " + num_1 + " + "+ num_2 + " + " + num_3);
+            System.out.println(nome + " Resulta em: " + media_aritimetica);
+        }else{
+            System.out.print ("Pressione qualquer tecla para terminar o programa ");
+            System.in.read();
+        }
+
+    }
+}
